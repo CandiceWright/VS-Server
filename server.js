ba3cc03a09db5fb30d10eaa33145047257fee8a2
Candice Wright <candicewright@Candices-MBP.attlocal.net>
2019-03-21 09:05:42 -0700

    update db code

--- server.js
+++ server.js
@@ -1,17 +1,49 @@
 //database setup
 var mysqldb = require('mysql');
 var bcrypt = require('bcrypt');
-var con = mysqldb.createConnection({
+// var con = mysqldb.createConnection({
+//   host: "vshootdb.cnubl3lw5gjo.us-west-2.rds.amazonaws.com",
+//   user: "root",
+//   password: "4912bU!L9",
+//   database: "vshootDB"
+// });
+
+// con.connect(function(err) {
+//   if (err) throw err;
+//   console.log("Connected!");
+// });
+
+var db_config = {
   host: "vshootdb.cnubl3lw5gjo.us-west-2.rds.amazonaws.com",
   user: "root",
   password: "4912bU!L9",
   database: "vshootDB"
-});
+}
+var connection;
 
-con.connect(function(err) {
-  if (err) throw err;
-  console.log("Connected!");
-});
+function handleDisconnect() {
+  connection = mysql.createConnection(db_config); // Recreate the connection, since
+                                                  // the old one cannot be reused.
+
+  connection.connect(function(err) {              // The server is either down
+    if(err) {                                     // or restarting (takes a while sometimes).
+      console.log('error when connecting to db:', err);
+      setTimeout(handleDisconnect, 2000); // We introduce a delay before attempting to reconnect,
+    }                                     // to avoid a hot loop, and to allow our node script to
+  });                                     // process asynchronous requests in the meantime.
+                                          // If you're also serving http, display a 503 error.
+  connection.on('error', function(err) {
+    console.log('db error', err);
+    if(err.code === 'PROTOCOL_CONNECTION_LOST') { // Connection to the MySQL server is usually
+      handleDisconnect();                         // lost due to either server restart, or a
+    } else {                                      // connnection idle timeout (the wait_timeout
+      throw err;                                  // server variable configures this)
+    }
+  });
+}
+
+handleDisconnect();
+
 
 //for sending mail
 var nodemailer = require('nodemailer');
