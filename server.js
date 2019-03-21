d65ff475b771a723adfe3583bdf2bf7b0875a76c
Candice Wright <candicewright@Candices-MacBook-Pro.local>
2019-03-21 11:13:39 -0700

    fix error

--- server.js
+++ server.js
@@ -19,20 +19,20 @@ var db_config = {
   password: "4912bU!L9",
   database: "vshootDB"
 }
-var connection;
+var con;
 
 function handleDisconnect() {
-  connection = mysqldb.createConnection(db_config); // Recreate the connection, since
+  con = mysqldb.createConnection(db_config); // Recreate the connection, since
                                                   // the old one cannot be reused.
 
-  connection.connect(function(err) {              // The server is either down
+  con.connect(function(err) {              // The server is either down
     if(err) {                                     // or restarting (takes a while sometimes).
       console.log('error when connecting to db:', err);
       setTimeout(handleDisconnect, 2000); // We introduce a delay before attempting to reconnect,
     }                                     // to avoid a hot loop, and to allow our node script to
   });                                     // process asynchronous requests in the meantime.
                                           // If you're also serving http, display a 503 error.
-  connection.on('error', function(err) {
+  con.on('error', function(err) {
     console.log('db error', err);
     if(err.code === 'PROTOCOL_CONNECTION_LOST') { // Connection to the MySQL server is usually
       handleDisconnect();                         // lost due to either server restart, or a
